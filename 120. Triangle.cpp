e07d67816381d4053f7e10fa73511eb57c503156
parth gupta <parthgupta0824@gmail.com>
2023-09-11 00:24:20 +0400

    new

--- 120. Triangle.cpp
+++ 120. Triangle.cpp
@@ -5,18 +5,76 @@ using namespace std;
 #define pb push_back
 #define vi vector<int>
 /*
-2
-3 4
-6 5 7
-4 1 8 3
-*/
+// ----------------------------------------- pure recursive code[TLE mare ga] --------------------------------
 int findans(int i, int j, vvi &v)
 {
-
+    if (j < 0 || j > v[i].size() - 1 || i < 0 || i > v.size() - 1)
+    {
+        return 0;
+    }
+    if (i == v.size() - 1)
+    {
+        return v[i][j];
+    }
+    int l = v[i][j] + findans(i + 1, j, v);
+    int r = v[i][j] + findans(i + 1, j + 1, v);
+    return min(l, r);
 }
+int minimumTotal(vvi &v)
+{
+    return findans(0, 0, v);
+}
+*/
+
+/*
+
+// ----------------------------------------- memoization --------------------------------
+int findans(int i, int j, vvi &v, vvi &dp)
+{
+    if (j < 0 || j > v[i].size() - 1 || i < 0 || i > v.size() - 1)
+    {
+        return 0;
+    }
+    if (i == v.size() - 1)
+    {
+        return v[i][j];
+    }
+    if (dp[i][j] != -1)
+    {
+        return dp[i][j];
+    }
+    int l = v[i][j] + findans(i + 1, j, v, dp);
+    int r = v[i][j] + findans(i + 1, j + 1, v, dp);
+    return dp[i][j] = min(l, r);
+}
+int minimumTotal(vvi &v)
+{
+    vvi dp(v.size(), vi(v[v.size() - 1].size(), -1));
+    return findans(0, 0, v, dp);
+}
+*/
+
+
+
+// ----------------------------------------- tabulation --------------------------------
 int minimumTotal(vector<vector<int>> &v)
 {
-    return findans(v.size() - 1, v[v.size() - 1].size() - 1, v);
+
+    vvi dp(v.size(), vi(v[v.size() - 1].size(), 0));
+
+    dp[v.size() - 1] = v[v.size() - 1];
+
+    for (int i = v.size() - 2; i >= 0; i--)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            int l = v[i][j] + dp[i + 1][j];
+            int r = v[i][j] + dp[i + 1][j + 1];
+            dp[i][j] = min(l, r);
+        }
+    }
+
+    return dp[0][0];
 }
 int main()
 {
@@ -39,16 +97,3 @@ int main()
     cout << minimumTotal(v);
     return 0;
 }
-
-
-/*
-                breakfast       lunch       snacks      dinner
-sunday             ok            na           ok          ok                3
-monday             ok            ok           ok          ok                4
-tuesday          ok/notok        ok           n-          ok                3
-wednesday          ok            |            ok          ok                3
-thursday           ok            -            n-          ok                2.5
-friday             ok            ok           n-           |                2.5
-saturday           ok            |            -         ok/notok            1.5
-
-*/
